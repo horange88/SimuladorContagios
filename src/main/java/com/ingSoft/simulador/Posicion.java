2c636350c1405e051b3f97b5d82ee6fc5c203d6a
Horacio <horange88@gmail.com>
2020-06-20 00:04:59 -0300

    posicion

--- src/main/java/com/ingSoft/simulador/Posicion.java
+++ src/main/java/com/ingSoft/simulador/Posicion.java
@@ -5,31 +5,44 @@ import java.util.Random;
 public class Posicion {
 	private int posx;
 	private int posy;
-	
-	public Posicion(Area area) {
-		Random r = new Random();
-		posx=r.nextInt(area.getAncho());
-		posy=r.nextInt(area.getAlto());
+	private Area area;
+
+	public Posicion() {
+		area = new Area(0,0);
+		posx = 0;
+		posy = 0;
 	}
-	
+
+	public Posicion(Area area) {
+		this.area = area;
+		Random r = new Random();
+		posx = r.nextInt(area.getAncho());
+		posy = r.nextInt(area.getAlto());
+	}
+
 	public int getPosx() {
 		return posx;
 	}
+
 	public void setPosx(int posx) {
 		this.posx = posx;
 	}
+
 	public int getPosy() {
 		return posy;
 	}
+
 	public void setPosy(int posy) {
 		this.posy = posy;
 	}
 
 	public void mover(Velocidad vel) {
-		posx += vel.getVelx();
-		posy += vel.getVely();
-		
-	}
+		if (posx + vel.getVelx() <= area.getAncho() && posx + vel.getVelx() >= 0)
+			posx += vel.getVelx();
 
+		if (posy + vel.getVely() <= area.getAlto() && posy + vel.getVely() >= 0)
+			posy += vel.getVely();
+
+	}
 
 }
